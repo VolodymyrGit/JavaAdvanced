1658feb7ffc6b80b1877c9e343a770a37cdd0a47
Melnyk_Volodymyr <olgreen123@gmail.com>
2021-03-11 11:01:37 +0200

    HW_1 we are nearing completion

--- src/main/java/homeWork1/Main.java
+++ src/main/java/homeWork1/Main.java
@@ -1,12 +1,18 @@
 package homeWork1;
 
+import org.apache.log4j.Logger;
+
 public class Main {
+
+    private static final Logger log = Logger.getLogger(String.valueOf(Main.class));
 
     public static void main(String[] args) {
 
+        log.info("This is my test");
+
 //        TeamLeadDao.addToTable(new TeamLead("John", "Doe", 45));
 
-        TeamLeadDao teamLeadDao = new TeamLeadDao();
-        teamLeadDao.findById(1);
+//        TeamLeadDao teamLeadDao = new TeamLeadDao();
+//        teamLeadDao.findById(1);
     }
 }
